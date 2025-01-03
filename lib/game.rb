b7a69aba1cf24f181992edef33f023a671421e70
Fahad Ahmed <fahad838@gmail.com>
2025-01-03 10:03:17 +0530

    Create game_menu method
    Game_menu method is the start menu of the game.
    It will have options to start game, Rules, Exit.
    
    Also created the get_choice method for the game_menu.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,30 @@
+# frozen_string_literal: true
+
 require './lib/board'
+require './lib/player'
+require './lib/messages'
 
 class Game
+  include Messages
+  attr_accessor :game_board, :players, :currently_playing
+
   def initialize
     @game_board = Board.new
+    @players = Player.new
+    @currently_playing = @players.player1
+  end
+
+  def game_menu
+    game_menu_text
+    choice = get_menu_choice
+  end
+
+  def get_menu_choice
+    puts "\nEnter your choice: "
+    gets.chomp.to_i
   end
 end
+
+
+g = Game.new
+g.game_menu
