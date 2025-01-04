d0dd5e34ae77cbd9aeb45369544db8dfe791442c
Fahad Ahmed <fahad838@gmail.com>
2025-01-04 10:20:49 +0530

    Test show_board method in board_spec.rb

--- lib/board.rb
+++ lib/board.rb
@@ -4,19 +4,14 @@ class Board
   def initialize
     rows = 6
     columns = 7
-    @board = Array.new(rows) { Array.new(columns, nil) }
+    @board = Array.new(rows) { Array.new(columns, '_') }
   end
 
   def show_board
     @board.each do |row|
-      puts row.map { |cell| cell || '.' }.join(' ')
+      puts row.map { |cell| cell }.join(' ')
     end
   end
 end
 
 
-b = Board.new
-b.show_board
-b.board[0][1] = 'X'
-puts
-b.show_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,7 +7,24 @@ describe Board do
     context 'when board class is initialized' do
       it 'create an array of 42 elements' do
         expect(board.board).to be_an_instance_of(Array)
-        expect(board.board.size).to eq(42)
+        expect(board.board.flatten.size).to eq(42)
+      end
+    end
+  end
+
+  describe '#show_board' do
+    context 'when show_board method is called' do
+      it 'prints the board correctly' do
+        expected_output = <<~BOARD
+          _ _ _ _ _ _ _
+          _ _ _ _ _ _ _
+          _ _ _ _ _ _ _
+          _ _ _ _ _ _ _
+          _ _ _ _ _ _ _
+          _ _ _ _ _ _ _
+        BOARD
+
+        expect { board.show_board }.to output(expected_output).to_stdout
       end
     end
   end
